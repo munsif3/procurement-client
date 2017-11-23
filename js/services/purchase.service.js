93533bd3a25d01cb6ac28d5c3f883ff13085cd7f
Saranki M <m.saranki@gmail.com>
2017-11-23 22:48:25 +0530

    Fetched employee details and binded

--- js/services/purchase.service.js
+++ js/services/purchase.service.js
@@ -25,6 +25,7 @@ function PurchaseOrderService($http) {
             return $http
                 .get("http://localhost:8080/api/purchaseOrders/purchaseId/" + purchaseId)
                 .then(function (response) {
+                    console.log(response.data);
                     return response.data;
               })
         },
@@ -37,52 +38,16 @@ function PurchaseOrderService($http) {
 
                     return response.data;
                 });
+        },
+        getLoggedUserDetails: function(username){
+            return $http
+            .get("http://localhost:8080/api/user/"+username)
+            .then(function (response) {
+                // console.log(response)
+                // console.log(response.data)
+
+                return response.data;
+            });
         }
     };
 }
-
-
-// angular.module('app').factory('AdminService',['$http','$q',function($http,$q){
-//   var factory={
-//       getAllDepartments:getAllDepartments,
-//       addDepartment:addDepartment,
-//       updateDepartment:updateDepartment,
-//       getAllSites:getAllSites,
-//       getAllProject:getAllProject,
-//       addProject:addProject,
-//       getProject:getProject,
-//       updateProject:updateProject,
-//       getAllUsres:getAllUsres,
-//       addUser:addUser
-//   };
-//   return factory;
-
-//   function getRequisitionOrderByStatus(){
-//     var deferred = $q.defer();
-//     $http.get("http://localhost:8080/purchaseOrders/requisitions")
-//         .then(
-//             function (response) {
-//                 deferred.resolve(response.data);
-//             },
-//             function(errResponse){
-//                 deferred.reject(errResponse);
-//             }
-//         );
-//     return deferred.promise;
-// }
-
-// }]);
-
-
-// // function PurchaseService($http) 
-// // {
-// //   return {
-// //     getRequisitionOrderByStatus: function() {
-// //       return $http
-// //         .get("http://localhost:8080/purchaseOrders/requisitions")
-// //         .then(function(response) {
-// //           return response.data;
-// //         });
-// //     }
-// //   };
-// // }
